157dbc457260825283f78113fef97ddd5f5f3f6f
Max Asensio <max.asensio13@gmail.com>
2026-05-13 13:36:53 +0200

    config for supabase storage

--- payload.config.ts
+++ payload.config.ts
@@ -1,30 +1,37 @@
 import sharp from "sharp";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { postgresAdapter } from "@payloadcms/db-postgres";
+import { s3Storage } from "@payloadcms/storage-s3";
 import { buildConfig } from "payload";
 import { Projects } from "./collections/projects";
 import { Media } from "./collections/media";
 import { Gallery } from "./collections/gallery";
 
 export default buildConfig({
-  // If you'd like to use Rich Text, pass your editor here
   editor: lexicalEditor(),
-
-  // Define and configure your collections in this array
   collections: [Projects, Media, Gallery],
-
-  // Your Payload secret - should be a complex and secure string, unguessable
   secret: process.env.PAYLOAD_SECRET || "",
-  // Whichever Database Adapter you're using should go here
-  // Mongoose is shown as an example, but you can also use Postgres
   db: postgresAdapter({
     pool: {
       connectionString: process.env.DATABASE_URL,
     },
   }),
-  // If you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-  // This is optional - if you don't need to do these things,
-  // you don't need it!
+  plugins: [
+    s3Storage({
+      collections: {
+        media: true,
+      },
+      bucket: "media",
+      config: {
+        endpoint: process.env.S3_ENDPOINT,
+        region: process.env.S3_REGION,
+        credentials: {
+          accessKeyId: process.env.S3_ACCESS_KEY_ID || "",
+          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || "",
+        },
+        forcePathStyle: true,
+      },
+    }),
+  ],
   sharp,
 });
